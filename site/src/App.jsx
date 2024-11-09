29dd1461d8576e4646007976a1c6861414c9502a
Deserteddd <eelis.autio98@gmail.com>
2024-11-09 12:44:43 +0200

    Fetch toimii

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,16 +1,35 @@
 import { useEffect, useState } from 'react'
-import ApiRequest from './components/ApiRequest'
 import CardContainer from './components/card_container';
 
 function App() {
   const [cards, setCards] = useState([]);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch('http://localhost:3000/api/data')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setCards(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setError(error.message);
+      });
+  }, []);
+
   return (
     <>
-      <ApiRequest />
+      <div>
+        <CardContainer cards = {cards}/>
+      </div>
     </>
   )
 }
 
-export default App
+
+export default App;
